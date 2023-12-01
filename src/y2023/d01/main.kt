82a44d6a89ed60bb951dac95f26a388e940ca7f1
Ernest Landrito <ernest@landrito.com>
2023-12-01 11:35:06 -0700

    y2023.d01 cleanup

--- src/y2023/d01/main.kt
+++ src/y2023/d01/main.kt
@@ -3,7 +3,6 @@ package y2023.d01
 import java.io.File
 import java.nio.file.Paths
 
-
 fun main() {
   val cwd = Paths.get("").toAbsolutePath().toString()
   val input = File("$cwd/src/y2023/d01/input.txt").readLines()
@@ -12,38 +11,54 @@ fun main() {
 }
 
 fun partOne(lines: List<String>): Int {
-  val isDigit = {c: Char -> c.toString().matches(Regex("[0-9]"))}
-  return lines.map {line ->  line.first(isDigit).toString() + line.last(isDigit)}
+  return lines.map { s ->
+    findDigit(s, DigitPosition.FIRST) + findDigit(s, DigitPosition.LAST)
+  }
     .map {digits ->  digits.toInt()}
     .reduce(Int::plus)
 }
 
 fun partTwo(lines: List<String>): Int {
-  // Since we only care about first and last digit we can just concatenate the ltr and rtl and we should get the
-  // correct digits at the start and end.
-  return lines.map{s -> replaceDigitWordsForDigitsLtr(s) + replaceDigitWordsForDigitsRtl(s)}.let(::partOne)
+  val findWords = true
+  return lines.map { s ->
+    findDigit(s, DigitPosition.FIRST, findWords) + findDigit(s, DigitPosition.LAST, findWords)
+  }
+    .map {digits ->  digits.toInt()}
+    .reduce(Int::plus)
+}
+
+data class Replace(val regexString: String, val replacement: String)
+
+fun String.replace(r: Replace): String {
+  return this.replace(Regex(r.regexString), r.replacement)
+}
+
+val DIGIT_REPLACEMENTS = arrayOf(
+  // Digit 0 is not specified as a spelled out digit in the spec.
+  Replace("one", "1"),
+  Replace("two", "2"),
+  Replace("three", "3"),
+  Replace("four", "4"),
+  Replace("five", "5"),
+  Replace("six", "6"),
+  Replace("seven", "7"),
+  Replace("eight", "8"),
+  Replace("nine", "9"),
+)
+
+enum class DigitPosition {
+  FIRST, LAST
+}
+
+fun findDigit(s: String, position: DigitPosition, findWords: Boolean = false): String {
+  val queryStrings = if(findWords) {
+    DIGIT_REPLACEMENTS.flatMap{ r -> listOf(r.regexString, r.replacement) }
+  } else DIGIT_REPLACEMENTS.map{ r -> r.replacement }
+  val result = if(position == DigitPosition.FIRST) s.findAnyOf(queryStrings) else s.findLastAnyOf(queryStrings)
+  val digitOrWord = result?.second ?: "0"
+  return digitOrWord.let(::replaceDigitWordsForDigits)
 }
 
 fun replaceDigitWordsForDigits(s: String): String {
-  data class Replace(val regexString: String, val replacement: String)
-  return arrayOf(
-    // Digit 0 is not specified as a spelled out digit in the spec.
-    Replace("one", "1"),
-    Replace("two", "2"),
-    Replace("three", "3"),
-    Replace("four", "4"),
-    Replace("five", "5"),
-    Replace("six", "6"),
-    Replace("seven", "7"),
-    Replace("eight", "8"),
-    Replace("nine", "9"),
-  ).fold(s) { result, replace -> result.replace(Regex(replace.regexString), replace.replacement) }
-}
-
-fun replaceDigitWordsForDigitsLtr(s: String): String {
-  return s.chunked(1).reduce {result, char -> (result + char).let(::replaceDigitWordsForDigits)}
-}
-
-fun replaceDigitWordsForDigitsRtl(s: String): String {
-  return s.chunked(1).reversed().reduce{result, char -> (char + result).let(::replaceDigitWordsForDigits)}
+  return DIGIT_REPLACEMENTS.fold(s) { result, replacement -> result.replace(replacement) }
 }
